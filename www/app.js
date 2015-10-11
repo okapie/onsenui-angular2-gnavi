c0801d5476beed9c175e46911a399e7a127d659c
okapie <tatigreuze@yahoo.co.jp>
2015-10-11 15:57:31 +0900

    [modify]Promise

--- www/app.js
+++ www/app.js
@@ -106,6 +106,20 @@ var SchedulePage = (function () {
 })();
 var AddItemPage = (function () {
     function AddItemPage(self, schedule) {
+        this.const = $http = {
+            get: function (url) {
+                return _sendRequest(url, null, 'GET');
+            },
+            post: function (url, payload) {
+                return _sendRequest(url, payload, 'POST');
+            },
+            put: function (url, payload) {
+                return _sendRequest(url, payload, 'PUT');
+            },
+            delete: function (url, payload) {
+                return _sendRequest(url, null, 'DELETE');
+            }
+        };
         this.element = self;
         this.schedule = schedule;
         this.times = [];
@@ -127,7 +141,29 @@ var AddItemPage = (function () {
         configurable: true
     });
     AddItemPage.prototype.addActivity = function () {
-        console.log("お気に入りに入れるボタンが押された" + testhttp);
+        console.log("addActivityが呼ばれた");
+        return new Promise(function (resolve, reject) {
+            var req = new XMLHttpRequest();
+            req.open(type, url);
+            req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            req.onload = function () {
+                if (req.status == 200) {
+                    resolve(JSON.parse(req.response));
+                }
+                else {
+                    reject(JSON.parse(req.response));
+                }
+            };
+            req.onerror = function () {
+                reject(JSON.parse(req.response));
+            };
+            if (payLoad) {
+                req.send(JSON.stringify(payLoad));
+            }
+            else {
+                req.send(null);
+            }
+        });
         /*
               navigator.geolocation.getCurrentPosition(
                   function(position){
@@ -172,7 +208,7 @@ var AddItemPage = (function () {
 })();
 // レストラン検索
 var searchRest = function () {
-    console.log("お気に入りに入れるボタンが押された");
+    //console.log("お気に入りに入れるボタンが押された");
     /*
     navigator.geolocation.getCurrentPosition(
         function(position){
--- www/app.ts
+++ www/app.ts
@@ -215,8 +215,49 @@ class AddItemPage {
     return node;
   }
 
+    export const $http = {
+    get: function(url: string) {
+        return _sendRequest(url, null, 'GET');
+    },
+    post: function(url: string, payload: any){
+        return _sendRequest(url, payload, 'POST');
+    },
+    put: function(url: string, payload: any){
+        return _sendRequest(url, payload, 'PUT');
+    },
+    delete: function(url: string, payload: any){
+        return _sendRequest(url, null, 'DELETE');
+    }
+}
+
   addActivity() {
-    console.log("お気に入りに入れるボタンが押された" + testhttp);
+
+  console.log("addActivityが呼ばれた");
+
+      return new Promise(function(resolve, reject) {
+          var req = new XMLHttpRequest();
+          req.open(type, url);
+          req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+          req.onload = function() {
+              if (req.status == 200) {
+                  resolve(JSON.parse(req.response));
+
+              } else {
+                  reject(JSON.parse(req.response));
+              }
+          };
+
+          req.onerror = function() {
+              reject(JSON.parse(req.response));
+          };
+
+          if (payLoad) {
+              req.send(JSON.stringify(payLoad));
+          } else {
+              req.send(null);
+          }
+      });
 
 
 /*
@@ -254,7 +295,7 @@ class AddItemPage {
 
 // レストラン検索
 var searchRest = function() {
-    console.log("お気に入りに入れるボタンが押された");
+    //console.log("お気に入りに入れるボタンが押された");
     /*
     navigator.geolocation.getCurrentPosition(
         function(position){
